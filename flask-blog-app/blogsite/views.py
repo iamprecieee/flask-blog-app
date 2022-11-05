e8b47571748894103547c3c32e42c8c2fb76e771
Precious Iheanyi <85397257+iamprecieee@users.noreply.github.com>
2022-11-05 17:45:17 +0100

    Add files via upload

---
+++ flask-blog-app/blogsite/views.py
@@ -0,0 +1,141 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+from flask_login import login_required, current_user
+from .models import Post, User, Comment, Like
+from . import db
+
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+@views.route("/home")
+def home():
+    posts = Post.query.all()
+    return render_template("home.html", user = current_user, posts = posts)
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        title = request.form.get('title')
+        text = request.form.get('text')
+
+        if not text and not title:
+            flash('Title and Text cannot be empty', category='error')
+        else:
+            post = Post(title=title, text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
+    return render_template("create_post.html")
+
+
+
+
+@views.route("/edit-post/<post_id>", methods=["GET", "POST"])
+@login_required
+def edit_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        text = request.form.get('text')
+
+        if not post:
+            flash("Post does not exist.", category='error')
+        elif current_user.id != post.author:
+            flash('You do not have permission to edit this post.', category='error')
+        else:
+            post.title = title
+            post.text = text 
+
+            db.session.commit()
+            flash('Post edited.', category='success')
+        return redirect(url_for('views.home'))
+    return render_template("edit.html", post=post)
+
+
+
+@views.route("/delete-post/<post_id>")
+@login_required
+def delete_post(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif current_user.id != post.author:
+        flash('You do not have permission to delete this post.', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted.', category='success')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/posts/<email>")
+@login_required
+def posts(email):
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash('No user with that email exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = user.posts
+    return render_template("posts.html", user=current_user, posts=posts, email=email)
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(
+        author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        return jsonify({'error': 'Post does not exist.'}, 400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
